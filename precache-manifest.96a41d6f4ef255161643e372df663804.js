7984c766c148563923fde0d2b9695f979c60bb09
bigSword <bigsword@gmail.com>
2018-11-20 13:31:47 +1300

    Updates

---
+++ precache-manifest.96a41d6f4ef255161643e372df663804.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "820768afa52bb8f1a0b4",
+    "url": "/react-calendar/static/js/runtime~main.820768af.js"
+  },
+  {
+    "revision": "d596fe9d750b48ec0a44",
+    "url": "/react-calendar/static/js/main.d596fe9d.chunk.js"
+  },
+  {
+    "revision": "f9b3ff8b8d385fa91918",
+    "url": "/react-calendar/static/js/1.f9b3ff8b.chunk.js"
+  },
+  {
+    "revision": "d596fe9d750b48ec0a44",
+    "url": "/react-calendar/static/css/main.5fe836fd.chunk.css"
+  },
+  {
+    "revision": "0f69a469c86e916e12484d36ad781060",
+    "url": "/react-calendar/index.html"
+  }
+];
